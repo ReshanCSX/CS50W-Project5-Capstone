41f5d9e154c83dfeec64236131ada8d47a4bdf0f
ReshanCSX <reshan.luck@gmail.com>
2023-09-12 19:39:51 +0530

    Combined name and location seached inputs

--- Frontend/src/components/HomePage/Card.jsx
+++ Frontend/src/components/HomePage/Card.jsx
@@ -1,14 +1,18 @@
 export default function Card(props){
     
     return (
-    <div className="border border-green-700">
+    <div className="shadow-md border rounded-md">
         <div>
-            <img className="max-h-20 w-full object-cover" alt={`image of the ${props.name}`} src="https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            <img className="h-40 w-full object-cover rounded-t-md" alt={`image of the ${props.name}`} src="https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
         </div>
-        <div className="p-3">
-            <h1 className="text-xl font-bold text-green-700">{props.name}</h1>
-            <h1 className="text-sm text-gray-600">{props.city}</h1>
-            <h3 className="font-bold text-base text-gray-600">{props.rating}/5</h3>
+        <div className="p-5">
+            <div className="flex justify-between">
+                <h1 className="text-lg font-bold text-slate-700">{props.name}</h1>
+                <h3 className="font-bold text-base text-gray-600"> Rating {props.rating}/5</h3>
+            </div>
+            <div>
+                <h1 className="text-sm text-gray-600">{props.location}</h1>
+            </div>
         </div>
     </div>
     )
