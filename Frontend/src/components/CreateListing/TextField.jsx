f9bb8794857beff5602cd64edc50b389ce2297bc
ReshanCSX <reshan.luck@gmail.com>
2023-09-19 14:52:31 +0530

    Error handling for the form

--- Frontend/src/components/CreateListing/TextField.jsx
+++ Frontend/src/components/CreateListing/TextField.jsx
@@ -1,7 +1,8 @@
 export default function TextField(props){
 
-    const {name, type, label, required, handleChange, value} = props
+    const {name, type, label, required, handleChange, value, errors} = props
 
+    
     return(
         <div className="mb-3">
             <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-700">{label}</label>
@@ -14,7 +15,7 @@ export default function TextField(props){
                 className="mb-2 w-full bg-gray-50 border border-green-600 text-gray-900 text-sm rounded p-2.5 focus:outline-green-700"
                 required={required ? true : false}
             />
-            <p className="text-red-500 text-xs">error</p>
+            {errors[name].length > 0 && <p className="text-red-500 text-xs">{errors[name]}</p>}
         </div>
     )
 }
