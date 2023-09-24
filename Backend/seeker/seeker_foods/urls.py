94420d01a1400dbbf136f42a063a5245d874ec83
ReshanCSX <reshan.luck@gmail.com>
2023-09-24 07:12:24 +0530

    User registration path

--- Backend/seeker/seeker_foods/urls.py
+++ Backend/seeker/seeker_foods/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     path('home', views.HomeView.as_view()),
     path('search', views.SearchView.as_view()),
     path('createlisting', views.CreatePlaceView.as_view()),
-    path('login', views.LoginView.as_view()),
+    path('register', views.RegisterView.as_view()),
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
 
 if settings.DEBUG:
