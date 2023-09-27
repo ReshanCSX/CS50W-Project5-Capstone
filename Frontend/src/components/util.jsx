120e122b619850f10a879e567f42427be23092f5
ReshanCSX <reshan.luck@gmail.com>
2023-09-27 13:14:23 +0530

    Location details plage design

--- Frontend/src/components/util.jsx
+++ Frontend/src/components/util.jsx
@@ -1,22 +1,23 @@
 import Star from "./Star"
 
-export function generateStars(rating){
+export function generateStars(rating, size){
+
     const stras=[]
     const numFullStars = Math.floor(rating)
     const hasHalfStars = (rating % 1 != 0)
     const starCount = numFullStars + (hasHalfStars ? 1 : 0)
 
     for(let i = 0; i < numFullStars; i++){
-        stras.push(<Star type={"fill"} key={`fillStar${i}`}/>)
+        stras.push(<Star type={"fill"} key={`fillStar${i}`} size={size}/>)
     }
 
     if(hasHalfStars){
-        stras.push(<Star type={"half-fill"} key="halfFill"/>)
+        stras.push(<Star type={"half-fill"} key="halfFill" size={size}/>)
     }
 
     if(starCount < 5){
         for(let i = 0; i < (5 -starCount); i++){
-            stras.push(<Star type={"empty"} key={`emptyStar${i}`}/>)
+            stras.push(<Star type={"empty"} key={`emptyStar${i}`} size={size}/>)
         }
     }
 
